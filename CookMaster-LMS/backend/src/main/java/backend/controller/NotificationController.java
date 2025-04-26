5c51d39a585942c727823a475020120828c77549
Shenith Madurawala <it22300096@my.sliit.lk>
2025-04-26 23:03:17 +0530

    Create NotificationController.java

--- CookMaster-LMS/backend/src/main/java/backend/controller/NotificationController.java
+++ CookMaster-LMS/backend/src/main/java/backend/controller/NotificationController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 import java.util.List;
 
 @RestController
